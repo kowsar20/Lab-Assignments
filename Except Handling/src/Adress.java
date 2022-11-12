1987ada31477a3ed910d28566ea56c23a3344c64
kowsar20 <113493881+kowsar20@users.noreply.github.com>
2022-11-12 23:48:49 +0600

    Add files via upload

---
+++ Except Handling/src/Adress.java
@@ -0,0 +1,24 @@
+
+public class Address {
+        private String name;
+        private String street;
+        private String city;
+        private String state;
+        private String zip;
+        public Address(String aName, String aStreet,
+                       String aCity, String aState, String aZip)
+        {
+            name = aName;
+            street = aStreet;
+            city = aCity;
+            state = aState;
+            zip = aZip;
+        }
+        public String format()
+        {
+            return name + "\n" + street + "\n"
+                    + city + ", " + state + " " + zip;
+        }
+    }
+
+}
